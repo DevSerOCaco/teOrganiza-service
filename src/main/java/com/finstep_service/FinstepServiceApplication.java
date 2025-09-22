e90a40aaf65dd1c4c6a6d917ea21ebb2f8ea8951
Paulo Ricardo Silva dos Santos Filho <pauloricardossfilho@gmail.com>
2025-09-21 21:43:04 -0300

    Primeiro commit projeto base spring

---
+++ src/main/java/com/finstep_service/FinstepServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finstep_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinstepServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinstepServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finstep_service/FinstepServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finstep_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinstepServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
